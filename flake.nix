b0287154f8fd6dd6ea4c52b0f287a8cbd0634026
Kevin French <fnivek@gmail.com>
2024-03-04 20:19:12 -0800

    FLAKE: Update the flake to include ./configuration.nix

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
+    };
   };
 }
