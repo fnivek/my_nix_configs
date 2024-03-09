0a8ffe03cca94855fe1be52b6758d8359b0aec9a
Kevin French <fnivek@gmail.com>
2024-03-09 13:23:08 -0800

    GIT: Add my lg aliases, enable lfs, set initial branch to main, use hx as the editor, and autocrlf = input

--- home.nix
+++ home.nix
@@ -111,6 +111,26 @@
     enable = true;
     userName = "Kevin French";
     userEmail = "fnivek@gmail.com";
+    aliases = {
+        la = "!git config -l | grep alias | cut -c 7-";
+
+        lg = "!git lg1";
+        lg1-all = "!git lg1 --all";
+        lg2-all = "!git lg2 --all";
+        lg3-all = "!git lg3 --all";
+
+        lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
+        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lg3 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+
+        last = "!git lg3-specific -1 HEAD --numstat";
+    };
+    lfs.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "hx";
+      core.autocrlf = "input";
+    };
   };
 
   # starship - an customizable prompt for any shell
