aab0be213523b7eb84d16b0757de95de61efc1e8
Kevin French <fnivek@gmail.com>
2024-03-11 20:33:07 -0700

    NUSHELL: Add nushell, use carapace as the command line completer and update starship

--- home.nix
+++ home.nix
@@ -141,10 +141,13 @@
     enable = true;
     # custom settings
     settings = {
-      add_newline = false;
+      add_newline = true;
       aws.disabled = true;
       gcloud.disabled = true;
-      line_break.disabled = true;
+      character = {
+         success_symbol = "[➜](bold green)";
+         error_symbol = "[➜](bold red)";
+      };
     };
   };
 
@@ -177,6 +180,41 @@
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
+  };
+
+  programs.nushell = {
+    enable = true;
+    extraConfig = ''
+       let carapace_completer = {|spans|
+       carapace $spans.0 nushell $spans | from json
+       }
+       $env.config = {
+        show_banner: false,
+        completions: {
+        case_sensitive: false # case-sensitive completions
+        quick: true    # set to false to prevent auto-selecting completions
+        partial: true    # set to false to prevent partial filling of the prompt
+        algorithm: "fuzzy"    # prefix or fuzzy
+        external: {
+        # set to false to prevent nushell looking into $env.PATH to find more suggestions
+            enable: true
+        # set to lower can improve completion performance at the cost of omitting some options
+            max_results: 100
+            completer: $carapace_completer # check 'carapace_completer'
+          }
+        }
+       }
+       $env.PATH = ($env.PATH |
+       split row (char esep) |
+       prepend /home/myuser/.apps |
+       append /usr/bin/env
+       )
+    '';
+  };
+
+  programs.carapace = {
+    enable = true;
+    enableNushellIntegration = true;
   };
 
   # This value determines the home Manager release that your
