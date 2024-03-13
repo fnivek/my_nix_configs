1151b88bf63237388f0f2c412d2d27e8fbaad57f
Kevin French <fnivek@gmail.com>
2024-03-12 20:36:58 -0700

    ZOXIDE: Install zoxide and use it in nushell

--- home.nix
+++ home.nix
@@ -186,6 +186,11 @@
     enable = true;
   };
 
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
