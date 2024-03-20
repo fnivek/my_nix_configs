b3082703fb93285a19b6af0c0770bebca236c56f
Kevin French <fnivek@gmail.com>
2024-03-19 20:23:14 -0700

    HYPRLAND: Set hyprland as default session

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.defaultSession = "hyprland";
 
   # Configure keymap in X11
   services.xserver = {
