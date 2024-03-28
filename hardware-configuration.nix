9ad5754cfa2688ca442ac5c879280121b6dbd627
Kevin French <fnivek@gmail.com>
2024-03-27 21:06:02 -0700

    HARDWARE: Add Hagrid's new hardware config after reinstall with swap space

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,17 +8,19 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/838acc58-abb4-468a-84e9-1b138bb5c6bc";
+    { device = "/dev/disk/by-uuid/bc77c5a4-f41a-4ece-9e7c-1fc4d54b5a09";
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/cb9babe6-8872-4abb-942e-0c1b18d85b18"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
