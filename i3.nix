550db6df3c45aa1399df3a0e1d386a458e112a76
Kevin French <fnivek@gmail.com>
2024-07-24 19:55:19 -0700

    I3: Send com apps to there workspace

--- i3.nix
+++ i3.nix
@@ -134,6 +134,25 @@ in {
         "${mod}+Shift+x" = "exec sh -c '${lock_cmd}'";
       });
 
+      window.commands = [
+        {
+          criteria = { title = "Todoist"; };
+          command = "move workspace 1:1:Com";
+        }
+        {
+          criteria = { title = "Gmail"; };
+          command = "move workspace 2:2:Com";
+        }
+        {
+          criteria = { title = "Calendar"; };
+          command = "move workspace 3:3:Com";
+        }
+        {
+          criteria = { title = "Music"; };
+          command = "move workspace 4:4:Com";
+        }
+      ];
+
       bars = [
         {
           colors = {
