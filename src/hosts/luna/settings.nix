571abc84f2ad388a7f1818ca37562f38a9c1f00b
Kevin French <fnivek@gmail.com>
2024-11-05 18:46:10 -0800

    LUNA: Add extra config for the i3 settings

--- src/hosts/luna/settings.nix
+++ src/hosts/luna/settings.nix
@@ -1,6 +1,12 @@
 _: {
   imports = [ ../host-settings.nix ];
-  hostSettings = {
-    hasBattery = true;
+  config = {
+    hostSettings = {
+      hasBattery = true;
+    };
+    xsession.windowManager.i3.extraConfig = ''
+      exec xrandr --setprovideroutputsource modesetting NVIDIA-0
+      exec xrandr --auto
+    '';
   };
 }
