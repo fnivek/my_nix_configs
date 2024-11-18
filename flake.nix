e197e0feaec06bc31066dedc61bf1b4d15e66f2e
Kevin French <fnivek@gmail.com>
2024-11-18 08:47:02 -0800

    NIXPKGS: Remove 23.11 and add unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs-23.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
@@ -21,7 +21,7 @@
   outputs =
     inputs@{
       nixpkgs,
-      nixpkgs-23,
+      nixpkgs-unstable,
       home-manager,
       i3_scripts,
       ...
@@ -31,6 +31,14 @@
         hostname:
         let
           system = "x86_64-linux";
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
         in
         {
           name = hostname;
@@ -59,6 +67,7 @@
                   # arguments to home.nix
                   extraSpecialArgs = {
                     inherit inputs;
+                    inherit pkgs-unstable;
                   };
                 };
               }
